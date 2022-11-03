33ed85cbb3d4dd20b64bfcab8d79f8c2f5ae99cb
Wendel Pineda <wendelpineda@mail.weber.edu>
2022-11-02 20:51:32 -0600

    Final Submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,45 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+import {gallery} from './movies'
+
+for (let m of gallery) {
+    let m_thumb = document.getElementById('c' + m.id);
+    m_thumb.innerHTML = `
+        <img src="${m.poster}" alt="${m.title}" class="img-thumbnail"/>
+    `
+    m_thumb.onclick = function(){
+        displayMovie(m);
+    }
+}
+let featured_movie = document.querySelector(".featured");
+
+function displayMovie(movie) {
+    featured_movie.innerHTML = `
+            <div class="card">
+            
+                <img src="${movie.poster}" class="card-img-top" alt="${movie.title}">
+    <div class="card-body">
+    <div class="card-header">${movie.title}</div>
+      <p class="card-text">"I have a pretty good memory, but memories are time beings, too, like cherry blossoms or ginkgo leaves; for a while they are beautiful and then they and die." </p>
+    </div>
+  </div>
+    `
+
+}
+function searchMovies(event){
+    event.preventDefault();
+    let input = document.querySelector('[type="search"]').value || "";
+    let count = 0;
+    for(let m of movies){
+        if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
+            document.querySelector('#m${m.id}').classList.add('d-none')
+        }
+        else{
+            document.querySelector('#m${m.id}').classList.remove('d-none')
+            count++;
+        }
+    }
+    featured_movie.innerHTML = count == 0 ? 'Nothing was found' : ''
+}
+
---
+++ src/javascripts/movies.js
@@ -0,0 +1,91 @@
+export const gallery = [{
+    "id": 1,
+    "title": "Cherry Tree 1",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    
+      "id": 2,
+      "title": "Cherry Tree 2",
+      "description": "Aesthetically Pleasing",
+      "poster": "https://images.unsplash.com/photo-1610687274720-bda24a3515a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+  
+    
+   
+  },
+  {
+    "id": 3,
+    "title": "Cherry Tree 3",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 4,
+    "title": "Cherry Tree 4",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 5,
+    "title": "Cherry Tree 5",
+    "description": "Aesthetically Pleasing",
+    "poster": "https://images.unsplash.com/photo-1610687274720-bda24a3515a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 6,
+    "title": "Cherry Tree 6",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 7,
+    "title": "Cherry Tree 7",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 8,
+    "title": "Cherry Tree 8",
+    "description": "Aesthetically Pleasing",
+    "poster": "https://images.unsplash.com/photo-1610687274720-bda24a3515a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 9,
+    "title": "Cherry Tree 9",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+  },
+  {
+    "id": 10,
+    "title": "Cherry Tree 10",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+  {
+    "id": 11,
+    "title": "Cherry Tree 11",
+    "description": "Aesthetically Pleasing",
+    "poster": "https://images.unsplash.com/photo-1610687274720-bda24a3515a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+
+  {
+    "id": 12,
+    "title": "Cherry Tree 12",
+    "description": "Cherry Blossom",
+    "poster": "https://images.unsplash.com/photo-1538943186303-104afadcbb16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+
+  },
+ 
+
+]
+  
